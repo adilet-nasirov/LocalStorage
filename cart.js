291cf77d55df82bafa65b73e8c4e03acc9c64efd
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-01-13 19:22:55 -0800

    new file cart css

--- cart.js
+++ cart.js
@@ -1,16 +1,42 @@
 import { getData } from "./data.js";
 import { showOnWindow } from "./helper.js";
+import { filter } from "./helper.js";
 const data = getData();
-const container = document.querySelector('.container');
-const span = document.querySelector('span')
+const container = document.querySelector(".container");
+const span = document.querySelector("span");
 
-const onLoad=()=>{
-    const arr=[]
-    for(let i=0; i< localStorage.length; i++){
-        let x=JSON.parse(localStorage.getItem(localStorage.key(i)));
-        arr.push(x)
+const onLoad = () => {
+  const arr = [];
+  const productsInCart = JSON.parse(localStorage.getItem("productsInCart"));
+  if (productsInCart !== null) {
+    const ids = Object.keys(productsInCart);
+    for (let el of ids) {
+      arr.push(filter(el));
     }
-    return arr;
-}
-const arrayFromLocal = onLoad()
-showOnWindow(arrayFromLocal)
+  }
+  return arr;
+};
+
+const showOnCartPage = (array) => {
+  for (let el of array) {
+    const div = document.createElement("div");
+    div.className = "card";
+    const img = document.createElement("img");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const button = document.createElement("button");
+
+    img.src = el.imageUrl;
+    h3.innerHTML = el.name;
+    p.innerText = `$${el.price}`;
+    button.innerHTML = "Delete";
+    button.id = el.id;
+    div.appendChild(img);
+    div.appendChild(button);
+    div.appendChild(h3);
+    div.appendChild(p);
+    container.appendChild(div);
+  }
+};
+const arrayFromLocal = onLoad();
+showOnCartPage(arrayFromLocal);
