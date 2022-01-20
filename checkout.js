0141e97dff21ea324a70f45084e0b703b949a348
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-01-19 16:47:08 -0800

    ////

--- checkout.js
+++ checkout.js
@@ -8,13 +8,15 @@ const displayItemsOnCart = (arr) => {
   const productsInCart = JSON.parse(localStorage.getItem("productsInCart"));
   const cartIcon=document.createElement('p')
   
-  cartIcon.innerHTML=`<h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b>4</b></span></h4>`
+  cartIcon.innerHTML=`<h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b id='num'>4</b></span></h4>`
   container.appendChild(cartIcon)
   for (let el of arr) {
     const p = document.createElement("p");
     p.innerHTML = `<a href="cart.html" id="">${el.name}</a> <span class="price">$${el.price}</span>`;
     container.appendChild(p);
   }
-  const totP=document.createElement('p')
+  const num= document.querySelector('#num')
+  num.innerHTML=arr.length;
+  // const totP=document.createElement('p')
 };
 displayItemsOnCart(onLoad());
